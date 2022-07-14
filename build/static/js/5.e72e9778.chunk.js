e0183cfc2ad8455007d50f9ad5ecd5d281149929
ocean-tech-developer <ocean.techsea@gmail.com>
2022-07-14 21:01:16 +0200

    fix popup

---
+++ build/static/js/5.e72e9778.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpchat=this.webpackJsonpchat||[]).push([[5],{1127:function(c,h){}}]);
+//# sourceMappingURL=5.e72e9778.chunk.js.map
---
+++ build/static/js/6.597954a9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpchat=this.webpackJsonpchat||[]).push([[6],{1248:function(c,h){}}]);
+//# sourceMappingURL=6.597954a9.chunk.js.map
